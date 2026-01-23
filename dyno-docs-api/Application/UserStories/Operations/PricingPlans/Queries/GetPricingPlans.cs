2f8ea589baa025007b416dfe6a1f8741e0b8f9b2
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2026-01-23 12:12:01 +0530

    feat: implement GetPricingPlans query and PricingPlanController for pricing plans retrieval

---
+++ dyno-docs-api/Application/UserStories/Operations/PricingPlans/Queries/GetPricingPlans.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities.Operations;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UserStories.Operations.PricingPlans.Queries;
+
+public class GetPricingPlans : IRequest<List<PricingPlan>> {}
+
+public class GetPricingPlansHandler(IApplicationDbContext context) : IRequestHandler<GetPricingPlans, List<PricingPlan>>
+{
+    public async Task<List<PricingPlan>> Handle(GetPricingPlans request, CancellationToken cancellationToken)
+    {
+        return await context.PricingPlan.ToListAsync(cancellationToken);
+    }
+}
---
+++ dyno-docs-api/UI/Controllers/Operations/PricingPlanController.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities.Operations;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers.Operations;
+
+[ApiController]
+[AllowAnonymous]
+[Route("api/operations/pricing-plans")]
+public class PricingPlanController (IMediator mediator) : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(List<PricingPlan>), 200)]
+    public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+    {
+        var plans = await mediator.Send(new Application.UserStories.Operations.PricingPlans.Queries.GetPricingPlans(), cancellationToken);
+        return Ok(plans);
+    }
+}
