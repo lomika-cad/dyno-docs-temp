b5dbfa5b05d108ac276195fdae1ef621f1289db6
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2026-02-13 19:32:37 +0530

    feat: implement GetUserSubscriptionByTenantId query and MeController for user subscription retrieval

---
+++ dyno-docs-api/Application/UserStories/Operations/UserSubscriptions/Queries/GetUserSubscriptionByTenantId.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities.Operations;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UserStories.Operations.UserSubscriptions.Queries;
+
+public class GetUserSubscriptionByTenantId : IRequest<UserSubscription>
+{
+    public Guid TenantId { get; set; }
+}
+
+public class GetUserSubscriptionByTenantIdHandler : IRequestHandler<GetUserSubscriptionByTenantId, UserSubscription>
+{
+    private readonly IApplicationDbContext _dbContext;
+
+    public GetUserSubscriptionByTenantIdHandler(IApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<UserSubscription> Handle(GetUserSubscriptionByTenantId request, CancellationToken cancellationToken)
+    {
+        var subscription = await _dbContext.UserSubscription
+            .FirstOrDefaultAsync(s => s.TenantId == request.TenantId, cancellationToken);
+
+        if (subscription == null)
+        {
+            throw new ApplicationException($"User subscription with id {request.TenantId} was not found");
+        }
+
+        return subscription;
+    }
+}
---
+++ dyno-docs-api/UI/Controllers/Operations/MeController.cs
@@ -0,0 +1,24 @@
+﻿using Application.UserStories.Operations.UserSubscriptions.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers.Operations;
+
+[ApiController]
+[Route("operations/me")]
+[Authorize]
+public class MeController (IMediator mediator) : ControllerBase
+{
+    [HttpGet("{tenantId}")]
+    [ProducesResponseType(typeof(object), 200)]
+    public async Task<IActionResult> GetMe(Guid tenantId, CancellationToken cancellationToken)
+    {
+        var query = new GetUserSubscriptionByTenantId()
+        {
+            TenantId = tenantId
+        };
+        var result = await mediator.Send(query, cancellationToken);
+        return Ok(result);
+    }
+}
