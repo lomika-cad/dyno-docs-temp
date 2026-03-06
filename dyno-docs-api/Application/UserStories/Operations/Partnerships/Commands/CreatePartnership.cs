47a51f9822279b8d5776b6ac80355855e4cc4657
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2026-03-06 21:06:55 +0530

    feat: add CRUD operations for partnerships with corresponding commands and controller

---
+++ dyno-docs-api/Application/UserStories/Operations/Partnerships/Commands/CreatePartnership.cs
@@ -0,0 +1,54 @@
+﻿using Application.Common;
+using Application.Common.Interfaces;
+using Domain.Common;
+using Domain.Entities.Operations;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using OneOf.Types;
+
+namespace Application.UserStories.Operations.Partnerships.Commands;
+
+public class CreatePartnership : IRequest<Result>
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? District { get; set; }
+    public PartnershipTypes PartnershipType { get; set; }
+    public IFormFile[]? Images { get; set; }
+}
+
+public class CreatePartnershipHandler (IApplicationDbContext dbContext) : IRequestHandler<CreatePartnership, Result>
+{
+    public async Task<Result> Handle(CreatePartnership request, CancellationToken cancellationToken)
+    {
+        byte[][] imageBytes = null;
+        if (request.Images != null)
+        {
+            var files = request.Images.Take(5).ToArray();
+            imageBytes = new byte[files.Length][];
+            for (int i = 0; i < files.Length; i++)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    await files[i].CopyToAsync(ms, cancellationToken);
+                    imageBytes[i] = ms.ToArray();
+                }
+            }
+        }
+        
+        var entity = new Partnership()
+        {
+            Name = request.Name,
+            Description = request.Description,
+            District = request.District,
+            PartnershipType = request.PartnershipType,
+            Images = imageBytes,
+            CreatedAt = DateTime.Now
+        };
+
+        dbContext.Partnership.Add(entity);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return Result.Success("Partnership created successfully");
+    }
+}
---
+++ dyno-docs-api/Application/UserStories/Operations/Partnerships/Queries/GetPartnershipList.cs
@@ -0,0 +1,17 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities.Operations;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UserStories.Operations.Partnerships.Queries;
+
+public class GetPartnershipList : IRequest<List<Partnership>> {}
+
+public class GetPartnershipListHandler(IApplicationDbContext dbContext) : IRequestHandler<GetPartnershipList, List<Partnership>>
+{
+    public async Task<List<Partnership>> Handle(GetPartnershipList request, CancellationToken cancellationToken)
+    {
+        var partnerships = await dbContext.Partnership.ToListAsync(cancellationToken);
+        return partnerships;
+    }
+}
